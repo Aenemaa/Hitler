709551f129e7df18e14277364cb6229c8012f544
Aenemaa <reinisskujenieks@gmail.com>
2022-03-02 08:48:32 +0200

    animestuff

--- animestuff.php
+++ animestuff.php
@@ -2,3 +2,12 @@ asdasdasdasd
 asdasdasdasdas
 <datagrid><datagrid><datagrid></datagrid></datagrid></datagrid>
 asdasdasdasddffgbvbv
+
+global config
+init
+add .
+ commit 
+ remote add origin
+ it push --set-upstream origin maste
+ git push
+ 
